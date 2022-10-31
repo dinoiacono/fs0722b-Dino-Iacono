c3184bd4c2103d9757f3ff205dab4b163fb4bf29
dinoiacono <dino.iacono.29@gmail.com>
2022-10-31 16:45:15 +0100

    esercitazionejs

---
+++ m1/w2/d1/js/naming.js
@@ -0,0 +1,21 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+
+var bottone = prompt ('sei maggiorenne?');
+console.log(bottone);
+    if(bottone='si'){
+         location.href = 'https://www.bernabei.it/';
+    }else{
+        location.href = 'https://www.peppapig.com/it-it';
+    }
+   
