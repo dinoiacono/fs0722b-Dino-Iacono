0e4b326e8b97b36b97e3bd4f3287475123a9044f
dinoiacono <dino.iacono.29@gmail.com>
2022-12-20 17:45:25 +0100

    esercizio m3 w1 d2

--- m3/w1/d2/prova.js
+++ m3/w1/d2/prova.js
@@ -1,2 +1,33 @@
-var ciao = 'hola';
-console.log(ciao);
+var balanceint = 1000;
+var SonAccount = /** @class */ (function () {
+    function SonAccount() {
+    }
+    SonAccount.prototype.Deposit = function (versamento) {
+        return balanceint + versamento;
+    };
+    SonAccount.prototype.WithDraw = function (prelievo) {
+        return balanceint - prelievo;
+    };
+    return SonAccount;
+}());
+var MotherAccount = /** @class */ (function () {
+    function MotherAccount() {
+    }
+    MotherAccount.prototype.Deposit = function (versamento) {
+        return balanceint + versamento;
+    };
+    MotherAccount.prototype.WithDraw = function (prelievo) {
+        return balanceint - prelievo;
+    };
+    MotherAccount.prototype.addInterest = function (interessi) {
+        return 'gli interessi sono di ' + (balanceint / 100) * interessi + '€';
+    };
+    return MotherAccount;
+}());
+var conto1 = new SonAccount();
+console.log(conto1.Deposit(100));
+console.log(conto1.WithDraw(50));
+var conto2 = new MotherAccount();
+console.log(conto2.Deposit(200));
+console.log(conto2.WithDraw(200));
+console.log(conto2.addInterest(10));
--- m3/w1/d2/prova.ts
+++ m3/w1/d2/prova.ts
@@ -1,2 +1,31 @@
-var ciao:string = 'hola';
-console.log(ciao);
+var balanceint:number = 1000;
+
+class SonAccount{
+    Deposit(versamento:number){
+        return balanceint+versamento;
+    }
+    WithDraw(prelievo:number) {
+        return balanceint-prelievo
+    }
+}
+
+class MotherAccount{
+    Deposit(versamento:number) {
+        return balanceint+versamento;
+    }
+    WithDraw(prelievo:number) {
+        return balanceint-prelievo
+    }
+    addInterest(interessi:number){
+        return 'gli interessi sono di ' +(balanceint/100)*interessi + '€'
+    }
+}
+
+const conto1 = new SonAccount();
+console.log(conto1.Deposit(100));
+console.log(conto1.WithDraw(50));
+
+const conto2 = new MotherAccount();
+console.log(conto2.Deposit(200));
+console.log(conto2.WithDraw(200));
+console.log(conto2.addInterest(10));
