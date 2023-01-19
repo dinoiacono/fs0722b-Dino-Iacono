05bcbce8e97c73f4ea4e24c9f6ad76abf44595c8
dinoiacono <dino.iacono.29@gmail.com>
2023-01-19 16:05:26 +0100

    creazione components e aggiunta files json

--- m3/w3/d5/esercizioSettimanale/src/app/app.module.ts
+++ m3/w3/d5/esercizioSettimanale/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SubscribeComponent } from './components/subscribe/subscribe.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FilmComponent } from './components/film/film.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SubscribeComponent,
+    HomeComponent,
+    NavbarComponent,
+    FilmComponent
   ],
   imports: [
     BrowserModule,
